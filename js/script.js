6e3d81a9e00af86c37e39e36d2b8037392a10636
Saqib-002 <saqibshah2882@gmail.com>
2025-08-17 08:31:57 +0500

    fixed the animation delay

--- js/script.js
+++ js/script.js
@@ -58,6 +58,73 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("profile-summary").innerHTML =
                 data.summary.profile;
 
+            // EXPERIENCE
+            data.experience.forEach((exp, index) => {
+                const delay = 0.4 + index * 0.2;
+                experienceList.innerHTML += `
+          <div class="experience-item hud-panel animate-slide-in-up" style="--delay: ${delay}s">
+            <div class="experience-title">
+              <h4>${exp.title}</h4>
+            </div>
+            <p class="experience-institute">${exp.company}</p>
+            <p class="experience-timeline">${exp.timeline}</p>
+            <p class="experience-description">
+              ${exp.description}
+            </p>
+          </div>
+        `;
+            });
+
+            // Projects
+            data.projects.forEach((project, index) => {
+                const delay = 0.6 + index * 0.2;
+                const techTags = project.techStack
+                    .map((tag) => `<div class="skill-tag">${tag}</div>`)
+                    .join("");
+                projectsList.innerHTML += `
+          <div class="hud-panel project-item animate-slide-in-up" style="--delay: ${delay}s">
+          <div>
+          <h4>${project.name}</h4>
+          <p>${project.description}</p>
+          </div>
+            <div class="project-tech">${techTags}</div>
+            <div class="project-hover-overlay">
+              <a href="${project.liveLink}" class="view-link" target="_blank">
+                <svg class="eye-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M12 4.5C7 4.5 2.73 7.61 0 12c2.73 4.39 7 7.5 12 7.5s9.27-3.11 12-7.5c-2.73-4.39-7-7.5-12-7.5zm0 13a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0-9a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7z" />
+                </svg>
+                <span>View Project</span>
+              </a>
+              <a href="${project.githubLink}" class="view-link" target="_blank">
+                <svg class="h-6 w-6 text-[#4b6b8b]" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+                <span>GitHub</span>
+              </a>
+            </div>
+          </div>
+        `;
+            });
+            
+            // SKILLS
+            data.skills.forEach((skillCat, index) => {
+                const skillsCatDiv = document.createElement("div");
+                skillsCatDiv.className = "skills-cat";
+                skillsCatDiv.innerHTML = `<h4>[${skillCat.category}]</h4>`;
+                const skillsItemDiv = document.createElement("div");
+                skillsItemDiv.className = "skill-item";
+                skillCat.items.forEach((skill, skillIndex) => {
+                    const delay = 0.8 + index * 0.1 + skillIndex * 0.05;
+                    skillsItemDiv.innerHTML += `
+            <div class="skill-tag animate-slide-in-up" style="--delay: ${delay}s">
+              ${skill}
+            </div>
+          `;
+                });
+                skillsCatDiv.appendChild(skillsItemDiv);
+                skillsMatrixContainer.appendChild(skillsCatDiv);
+            });
+
             // EDUCATION
             data.education.forEach((edu, index) => {
                 const educationItem = document.createElement("div");
@@ -88,44 +155,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 educationList.appendChild(educationItem);
             });
 
-            // SKILLS
-            data.skills.forEach((skillCat, index) => {
-                const skillsCatDiv = document.createElement("div");
-                skillsCatDiv.className = "skills-cat";
-                skillsCatDiv.innerHTML = `<h4>[${skillCat.category}]</h4>`;
-                const skillsItemDiv = document.createElement("div");
-                skillsItemDiv.className = "skill-item";
-                skillCat.items.forEach((skill, skillIndex) => {
-                    const delay = 0.6 + index * 0.1 + skillIndex * 0.05;
-                    skillsItemDiv.innerHTML += `
-            <div class="skill-tag animate-slide-in-up" style="--delay: ${delay}s">
-              ${skill}
-            </div>
-          `;
-                });
-                skillsCatDiv.appendChild(skillsItemDiv);
-                skillsMatrixContainer.appendChild(skillsCatDiv);
-            });
-
-            // EXPERIENCE
-            data.experience.forEach((exp, index) => {
-                const delay = 0.8 + index * 0.2;
-                experienceList.innerHTML += `
-          <div class="experience-item hud-panel animate-slide-in-up" style="--delay: ${delay}s">
-            <div class="experience-title">
-              <h4>${exp.title}</h4>
-            </div>
-            <p class="experience-institute">${exp.company}</p>
-            <p class="experience-timeline">${exp.timeline}</p>
-            <p class="experience-description">
-              ${exp.description}
-            </p>
-          </div>
-        `;
-            });
             // Certifications
             data.certifications.forEach((cert, index) => {
-                const delay = 1 + index * 0.1;
+                const delay = 1.2 + index * 0.1;
                 certificationsList.innerHTML += `
           <div
             class="hud-panel certification-item animate-slide-in-up"
@@ -139,36 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
           </div>
         `;
             });
-            // Projects
-            data.projects.forEach((project, index) => {
-                const delay = 1.2 + index * 0.2;
-                const techTags = project.techStack
-                    .map((tag) => `<div class="skill-tag">${tag}</div>`)
-                    .join("");
-                projectsList.innerHTML += `
-          <div class="hud-panel project-item animate-slide-in-up" style="--delay: ${delay}s">
-          <div>
-          <h4>${project.name}</h4>
-          <p>${project.description}</p>
-          </div>
-            <div class="project-tech">${techTags}</div>
-            <div class="project-hover-overlay">
-              <a href="${project.liveLink}" class="view-link" target="_blank">
-                <svg class="eye-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12 4.5C7 4.5 2.73 7.61 0 12c2.73 4.39 7 7.5 12 7.5s9.27-3.11 12-7.5c-2.73-4.39-7-7.5-12-7.5zm0 13a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0-9a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7z" />
-                </svg>
-                <span>View Project</span>
-              </a>
-              <a href="${project.githubLink}" class="view-link" target="_blank">
-                <svg class="h-6 w-6 text-[#4b6b8b]" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                </svg>
-                <span>GitHub</span>
-              </a>
-            </div>
-          </div>
-        `;
-            });
+            
         })
         .catch((err) => console.log("error fetting data", err));
     function getSocialSvg(platform) {
